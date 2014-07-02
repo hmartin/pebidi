99daa735841d39511b9d18934c308a8ccd4b7fc8
hmartin <hmartin.fr@gmail.com>
2014-07-02 20:39:36 +0200

    focus

--- web/js/filters.js
+++ web/js/filters.js
@@ -10,3 +10,16 @@ app
         }
     });
 ;
+
+
+
+app.directive('autoFocus', function($timeout) {
+    return {
+        restrict: 'AC',
+        link: function(_scope, _element) {
+            $timeout(function(){
+                _element[0].focus();
+            }, 0);
+        }
+    };
+});
