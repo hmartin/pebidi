74c1f7fe4978c3ca01ea3e2ca52060b7f17f4bdf
hmartin <hmartin.fr@gmail.com>
2015-01-30 17:22:21 +0000

    Def

--- src/Main/DefaultBundle/Entity/WordWord.php
+++ src/Main/DefaultBundle/Entity/WordWord.php
@@ -9,7 +9,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
  * @ORM\Table()
  * @ORM\Entity
  */
-class WordWord
+class Definition
 {
     /**
      * @ORM\Id
@@ -20,70 +20,9 @@ class WordWord
 
     /**
      * @ORM\ManyToOne(targetEntity="Word", inversedBy="translations",cascade={"persist"})
-     * @ORM\JoinColumn(name="word1_id", referencedColumnName="id")
+     * @ORM\JoinColumn(name="word_id", referencedColumnName="id")
      */
-    protected $word1;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="Word", inversedBy="translations",cascade={"persist"})
-     * @ORM\JoinColumn(name="word2_id", referencedColumnName="id")
-     */
-    protected $word2;
+    protected $word;
 
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set word1
-     *
-     * @param \Main\DefaultBundle\Entity\Word $word1
-     * @return WordWord
-     */
-    public function setWord1(\Main\DefaultBundle\Entity\Word $word1 = null)
-    {
-        $this->word1 = $word1;
-
-        return $this;
-    }
-
-    /**
-     * Get word1
-     *
-     * @return \Main\DefaultBundle\Entity\Word 
-     */
-    public function getWord1()
-    {
-        return $this->word1;
-    }
-
-    /**
-     * Set word2
-     *
-     * @param \Main\DefaultBundle\Entity\Word $word2
-     * @return WordWord
-     */
-    public function setWord2(\Main\DefaultBundle\Entity\Word $word2 = null)
-    {
-        $this->word2 = $word2;
-
-        return $this;
-    }
-
-    /**
-     * Get word2
-     *
-     * @return \Main\DefaultBundle\Entity\Word 
-     */
-    public function getWord2()
-    {
-        return $this->word2;
-    }
 }
