74cff9dd7ca15e2b1d61ab09dc22bb3de63648f8
hmartin <hmartin.fr@gmail.com>
2015-09-17 07:15:57 +0000

    chrome extension

---
+++ pebidiForChrome/content_script.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener('selectionchange', function() {
+    var selection = window.getSelection().toString().trim();
+    chrome.runtime.sendMessage({
+        request: 'updateContextMenu',
+        selection: selection
+    });
+});
